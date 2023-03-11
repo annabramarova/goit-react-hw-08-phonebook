2638e7f210d7b239e3bad7f9ba2de570df6f76e5
Anna Bramarova <annabramarova@gmail.com>
2023-03-11 19:29:15 +0200

    fix:contacts page

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,84 +1,17 @@
 import { useSelector } from 'react-redux';
-import { Flex, Spinner } from '@chakra-ui/react';
-import { selectContactsAmount, selectError, selectFilter, selectFilteredContacts, selectFilteredTotalAmount, selectIsLoading } from 'redux/selectors';
+import { Flex,  } from '@chakra-ui/react';
+import { selectFilteredContacts } from 'redux/selectors';
 import { ContactItem } from 'components/ContactItem';
 
 export const ContactList = () => {
-  const filtered = useSelector(selectFilteredContacts);
-  const error = useSelector(selectError);
-  const isLoading = useSelector(selectIsLoading);
-  const contactsAmount = useSelector(selectContactsAmount);
-  const filter = useSelector(selectFilter);  
-  const filteredAmount = useSelector(selectFilteredTotalAmount);
-  
+  const filtered = useSelector(selectFilteredContacts);  
 
   return (
-    <>
-      {isLoading && <Spinner color="gray.300" />}  
-      {error && <Flex alignItems='center' justifyContent='center' color="red">{error}</Flex>}
-      {contactsAmount > 0 ?
-        <Flex as="ul" direction="column" gap={4} alignItems='center' justifyContent='center'>
+      <Flex as="ul" direction="column" gap={4} alignItems='center' justifyContent='center'>
         {filtered?.map(contact => (
           <ContactItem key={contact.id} contact={contact}></ContactItem>
         ))}
       </Flex>
-        : <Flex alignItems='center' justifyContent='center' color="red">Contacts list is empty</Flex>}
-      {filter.length !== 0 && filteredAmount === 0 && <Flex alignItems='center' justifyContent='center' color="red">Contact not found. Please try again or add a new one</Flex> }
-    </>
   );
 };
 
-
-// import { Fragment, useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { deleteContact, fetchContacts } from 'redux/contacts/contactsOperations';
-// import { selectContactsAmount, selectError, selectFilteredContacts,  selectFilteredTotalAmount} from 'redux/selectors';
-
-// import {
-//   List,
-//   ListItem,
-//   ListText,
-//   Button,
-//   ListEmpty,
-// } from './ContactList.styled';
-
-
-// const ContactList = () => {
-//   const filtered = useSelector(selectFilteredContacts);
-//   const dispatch = useDispatch();
-//   // const isLoading = useSelector(selectIsLoading);
-//   const error = useSelector(selectError);
-//   const contactsAmount = useSelector(selectContactsAmount);
-//   const filteredAmount = useSelector(selectFilteredTotalAmount);
-
-//   useEffect(() => {
-//     dispatch(fetchContacts());
-//   }, [dispatch]);
-  
-
-//   return (
-//     <Fragment>
-//       {error && <p>{error}</p>}
-//       {contactsAmount === 0 && <ListEmpty>Contacts list is empty</ListEmpty>}
-//       {filteredAmount=== 0 && <ListEmpty>Contact not found. Please try again or add a new one</ListEmpty>}
-//       <List>
-//           {filtered.map(({ id, name, number }) => (
-//             <ListItem key={id}>
-//               <ListText>{name}:  {number} </ListText>
-//               <Button type="button"
-//                 onClick={() => dispatch(deleteContact(id))}
-//               >
-//                 Delete
-//               </Button>
-//             </ListItem>
-//           ))}
-//         </List>
-//     </Fragment>
-//   );
-// };
-
-// ContactList.defaultProps = {
-//   contacts: [],
-// };
-
-// export default ContactList;
--- src/pages/ContactsPage/ContactsPage.js
+++ src/pages/ContactsPage/ContactsPage.js
@@ -4,7 +4,7 @@ import { Box, Flex, Heading, Spinner } from '@chakra-ui/react';
 
 import { useDispatch, useSelector } from "react-redux";
 import { fetchContacts } from "redux/contacts/contactsOperations";
-import { selectIsLoading } from "redux/selectors";
+import { selectContactsAmount, selectError, selectFilter, selectFilteredTotalAmount, selectIsLoading } from "redux/selectors";
 
 import { ContactForm } from "components/ContactForm";
 import {ContactList} from "components/ContactList";
@@ -14,6 +14,10 @@ import { Filter } from "components/Filter";
 export default function ContactsPage() {
     const dispatch = useDispatch();
     const isLoading = useSelector(selectIsLoading);
+    const error = useSelector(selectError);
+    const contactsAmount = useSelector(selectContactsAmount);
+    const filter = useSelector(selectFilter);  
+    const filteredAmount = useSelector(selectFilteredTotalAmount);
 
     useEffect(() => {
         dispatch(fetchContacts());
@@ -30,10 +34,13 @@ export default function ContactsPage() {
             <ContactForm />
             <Box>
             <Filter />
-            <Flex justify="center" w="300px" mb={2}>
-            {isLoading && <Spinner color="gray.300" />}
+            <Flex alignItems='center' justifyContent='center' mb={2}>
+            {isLoading && <Spinner color="gray.300" mb={2} />}
             </Flex>
-                <ContactList />
+            {error && <Flex alignItems='center' justifyContent='center' color="red">{error}</Flex>}
+            {contactsAmount > 0 ? <ContactList />
+            : <Flex alignItems='center' justifyContent='center' color="red">Contacts list is empty</Flex>}
+            {filter.length !== 0 && filteredAmount === 0 && <Flex alignItems='center' justifyContent='center' color="red">Contact not found. Please try again or add a new one</Flex> }
             </Box>
         </Fragment>
     )
