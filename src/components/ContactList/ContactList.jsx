25977eef6a85b17aa3ff1101cd489b3c98407b5d
Anna Bramarova <annabramarova@gmail.com>
2023-03-11 20:50:47 +0200

    feat: sort alphabetical of contacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,10 +5,14 @@ import { ContactItem } from 'components/ContactItem';
 
 export const ContactList = () => {
   const filtered = useSelector(selectFilteredContacts);  
+  const sortedContacts = [...filtered].sort((a, b) =>
+    a.name.localeCompare(b.name)
+  );
+
 
   return (
       <Flex as="ul" direction="column" gap={4} alignItems='center' justifyContent='center'>
-        {filtered?.map(contact => (
+        {sortedContacts?.map(contact => (
           <ContactItem key={contact.id} contact={contact}></ContactItem>
         ))}
       </Flex>
